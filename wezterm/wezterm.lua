3267e00d9b34ac5892e477bb775cee2f492a3b4a
riku929hr <33782267+riku929hr@users.noreply.github.com>
2024-04-04 22:00:15 +0900

    apply stylua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,17 +1,15 @@
 -- Pull in the wezterm API
-local wezterm = require("wezterm")
+local wezterm = require "wezterm"
 
 -- This table will hold the configuration.
 local config = {}
 
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+if wezterm.config_builder then config = wezterm.config_builder() end
 
 -- This is where you actually apply your config choices
-config.font = wezterm.font_with_fallback({ "Monaspace Neon", "Hiragino Sans", "Yu Gothic" })
+config.font = wezterm.font_with_fallback { "Monaspace Neon", "Hiragino Sans", "Yu Gothic" }
 config.font_size = 13.2
 
 -- Color Scheme
