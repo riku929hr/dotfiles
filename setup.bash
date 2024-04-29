5e0d2bcf2e78755e5d5290e4785704675f2c1b14
riku929hr <33782267+riku929hr@users.noreply.github.com>
2024-04-29 11:26:27 +0900

    apply shellcheck

--- setup.bash
+++ setup.bash
@@ -1,51 +1,51 @@
 #!/usr/bin/env bash
 
-DOTFILES=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+DOTFILES=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
-mkdir -p $HOME/.config
+mkdir -p "$HOME"/.config
 
 # homebrew
-rm -rf $HOME/Brewfile
-ln -s $DOTFILES/homebrew/Brewfile $HOME/Brewfile
+rm -rf "$HOME"/Brewfile
+ln -s "$DOTFILES"/homebrew/Brewfile "$HOME"/Brewfile
 
 # zsh
-rm -rf $HOME/.zshrc
-ln -s $DOTFILES/zsh/zshrc $HOME/.zshrc
+rm -rf "$HOME"/.zshrc
+ln -s "$DOTFILES"/zsh/zshrc "$HOME"/.zshrc
 
 # prezto
-rm -rf $HOME/.zpreztorc
-ln -s $DOTFILES/zsh/zpreztorc $HOME/.zpreztorc
+rm -rf "$HOME"/.zpreztorc
+ln -s "$DOTFILES"/zsh/zpreztorc "$HOME"/.zpreztorc
 
 # kitty
-rm -rf $HOME/.config/kitty
-ln -s $DOTFILES/kitty $HOME/.config/kitty
+rm -rf "$HOME"/.config/kitty
+ln -s "$DOTFILES"/kitty "$HOME"/.config/kitty
 
 # wezterm
-rm -rf $HOME/.config/wezterm
-ln -s $DOTFILES/wezterm $HOME/.config/wezterm
+rm -rf "$HOME"/.config/wezterm
+ln -s "$DOTFILES"/wezterm "$HOME"/.config/wezterm
 
 # tmux
-rm -rf $HOME/.tmux.conf
-ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
+rm -rf "$HOME"/.tmux.conf
+ln -s "$DOTFILES"/tmux/tmux.conf "$HOME"/.tmux.conf
 
 # lazygit
-rm -rf $HOME/.config/lazygit
-ln -s $DOTFILES/lazygit $HOME/.config/lazygit
+rm -rf "$HOME"/.config/lazygit
+ln -s "$DOTFILES"/lazygit "$HOME"/.config/lazygit
 
 # git
-mkdir -p $HOME/.config/git
-rm -rf $HOME/.config/git
-ln -s $DOTFILES/git $HOME/.config/git
+mkdir -p "$HOME"/.config/git
+rm -rf "$HOME"/.config/git
+ln -s "$DOTFILES"/git "$HOME"/.config/git
 
 # commands
-mkdir -p $HOME/.local/bin
-rm -rf $HOME/.local/bin/t
-ln -s $DOTFILES/scripts/t $HOME/.local/bin/t
+mkdir -p "$HOME"/.local/bin
+rm -rf "$HOME"/.local/bin/t
+ln -s "$DOTFILES"/scripts/t "$HOME"/.local/bin/t
 
 # neovim
-rm -rf $HOME/.config/nvim
-ln -s $DOTFILES/nvim $HOME/.config/nvim
+rm -rf "$HOME"/.config/nvim
+ln -s "$DOTFILES"/nvim "$HOME"/.config/nvim
 
 # npm
-rm -rf $HOME/.default-npm-packages
-ln -s $DOTFILES/npm/default-npm-packages $HOME/.default-npm-packages
+rm -rf "$HOME"/.default-npm-packages
+ln -s "$DOTFILES"/npm/default-npm-packages "$HOME"/.default-npm-packages
