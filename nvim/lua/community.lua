adf7431e5f098dff03a0baf560ede5eb6387d497
riku929hr <33782267+riku929hr@users.noreply.github.com>
2024-07-25 23:59:37 +0900

    use copilot-lua-cmp and add rust lang pack

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -5,22 +5,13 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  -- language packages
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.completion.copilot-lua" },
-  {
-    "copilot.lua",
-    opts = {
-      suggestion = {
-        keymap = {
-          accept = "<C-l>",
-          accept_word = "<C-k>",
-          accept_line = false,
-          next = "<C-.>",
-          prev = "<C-,>",
-          dismiss = "<C/>",
-        },
-      },
-    },
-  },
+  { import = "astrocommunity.pack.rust" },
+
+  -- completion
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+
+  -- editing support
   { import = "astrocommunity.editing-support.refactoring-nvim" },
 }
