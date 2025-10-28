4a50d2cd47a86179e8eba19f59be1b2e12bd7fd5
riku929hr <33782267+riku929hr@users.noreply.github.com>
2025-10-28 21:50:26 +0900

    update setup

--- setup.bash
+++ setup.bash
@@ -20,39 +20,33 @@ ln -s "$DOTFILES"/zsh/zshrc "$HOME"/.zshrc
 rm -rf "$HOME"/.zpreztorc
 ln -s "$DOTFILES"/zsh/zpreztorc "$HOME"/.zpreztorc
 
-# kitty
+# config directory symlinks
 rm -rf "$HOME"/.config/kitty
 ln -s "$DOTFILES"/kitty "$HOME"/.config/kitty
 
-# wezterm
 rm -rf "$HOME"/.config/wezterm
 ln -s "$DOTFILES"/wezterm "$HOME"/.config/wezterm
 
-# mise
+mkdir -p "$HOME"/.config/mise
 rm -rf "$HOME"/.config/mise/config.toml
 ln -s "$DOTFILES"/mise/config.toml "$HOME"/.config/mise/config.toml
 
-# tmux
-rm -rf "$HOME"/.tmux.conf
-ln -s "$DOTFILES"/tmux/tmux.conf "$HOME"/.tmux.conf
-
-# lazygit
 rm -rf "$HOME"/.config/lazygit
 ln -s "$DOTFILES"/lazygit "$HOME"/.config/lazygit
 
-# git
-mkdir -p "$HOME"/.config/git
 rm -rf "$HOME"/.config/git
 ln -s "$DOTFILES"/git "$HOME"/.config/git
 
-# commands
+rm -rf "$HOME"/.config/nvim
+ln -s "$DOTFILES"/nvim "$HOME"/.config/nvim
+
+# other locations
+rm -rf "$HOME"/.tmux.conf
+ln -s "$DOTFILES"/tmux/tmux.conf "$HOME"/.tmux.conf
+
 mkdir -p "$HOME"/.local/bin
 rm -rf "$HOME"/.local/bin/t
 ln -s "$DOTFILES"/scripts/t "$HOME"/.local/bin/t
-
-# neovim
-rm -rf "$HOME"/.config/nvim
-ln -s "$DOTFILES"/nvim "$HOME"/.config/nvim
 
 # npm
 rm -rf "$HOME"/.default-npm-packages
